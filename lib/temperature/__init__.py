c00b7e040fa25ad348e2536bae19440ea3ca9b50
JLBBARCO <joseluizbarco@gmail.com>
2025-08-16 11:55:41 -0300

    Update temperature

--- lib/temperature/__init__.py
+++ lib/temperature/__init__.py
@@ -12,7 +12,7 @@ while True:
     resposta1 = int(input('De qual temperatura quer converter? '))
 
     if resposta1 == 0:
-        print('Saindo do conversor...')
+        print('Voltando...')
         break
     
     else:
@@ -23,7 +23,7 @@ while True:
                 break
 
             elif resposta2 == 1:
-                print('\033[33mAs unidades de origem e destino são iguais. Nenhuma conversão necessária.\033[m')
+                print('\033[31mAs unidades de origem e destino são iguais. Nenhuma conversão necessária.\033[m')
                 
             elif resposta2 == 2:
                 conversão = float(input('Digite a temperatura em Celsius: '))
@@ -33,7 +33,10 @@ while True:
             elif resposta2 == 3:
                 conversão = float(input('Digite a temperatura em Celsius: '))
                 resultado = conversão + 273.15
-                print(f"A temperatura em Kelvin é: {resultado:.2f} K")
+                if resultado < 0:
+                    print('\033[31mERRO!\033[m A operação não pode ser concluída pois o resultado é abaixo de zero, e Kelvin possui zero ABSOLUTO.')
+                else:
+                    print(f"A temperatura em Kelvin é: {resultado:.2f} K")
                 
             else:
                 print('\033[31mERRO! Digite uma opção válida.\033[m')
@@ -49,12 +52,15 @@ while True:
                 print(f"A temperatura em Celsius é: {resultado:.2f} °C")
                 
             elif resposta2 == 2:
-                print('\033[33mAs unidades de origem e destino são iguais. Nenhuma conversão necessária.\033[m')
+                print('\033[31mAs unidades de origem e destino são iguais. Nenhuma conversão necessária.\033[m')
                 
             elif resposta2 == 3:
                 conversão = float(input('Digite a temperatura em Fahrenheit: '))
                 resultado = (conversão - 32) * 5/9 + 273.15
-                print(f"A temperatura em Kelvin é: {resultado:.2f} K")
+                if resultado < 0:
+                    print('\033[31mERRO!\033[m A operação não pode ser concluída pois o resultado é abaixo de zero, e Kelvin possui zero ABSOLUTO.')
+                else:
+                    print(f"A temperatura em Kelvin é: {resultado:.2f} K")
                 
             else:
                 print('\033[31mERRO! Digite uma opção válida.\033[m')
@@ -75,7 +81,7 @@ while True:
                 print(f"A temperatura em Fahrenheit é: {resultado:.2f} °F")
 
             elif resposta2 == 3:
-                print('\033[33mAs unidades de origem e destino são iguais. Nenhuma conversão necessária.\033[m')
+                print('\033[31mAs unidades de origem e destino são iguais. Nenhuma conversão necessária.\033[m')
 
             else:
                 print('\033[31mERRO! Digite uma opção válida.\033[m')
